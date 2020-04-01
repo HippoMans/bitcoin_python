105703184bb9317070b49e8b5337fd4d6b17124c
HippoMans <army1703@pukyong.ac.kr>
2020-04-02 01:21:50 +0900

    __rmul__()함수 추가

--- src/FieldElement.py
+++ src/FieldElement.py
@@ -1,4 +1,5 @@
 class FieldElement:
+#FieldElement 객체에 내부값을 초기화(생성자)
     def __init__(self, num, prime):
         if num >= prime or num < 0:
             error = 'Num {} not in filed range 0 to {}'.format(num, prime-1)
@@ -6,44 +7,57 @@ class FieldElement:
         self.num = num
         self.prime = prime
 
+#어떤 객체를 인자로 하면 객체의 클래스에 정의된 __repr__를 실행하여 출력
     def __repr__(self):
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
+#두 객체가 같은지 확인
     def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
 
+#두 객체가 다른지 확인
     def __ne__(self, other):
         if other is None:
             return False
         return not (self == other)
 
+#두 객체의 합
     def __add__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = (self.num + other.num) % self.prime
         return self.__class__(num, self.prime)
 
+#두 객체의 차
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = (self.num - other.num) % self.prime
         return self.__class__(num, self.prime)
 
+#두 객체의 곱
     def __mul__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = (self.num * other.num) % self.prime
         return self.__class__(num, self.prime)
 
+#두 객체의 제곱
     def __pow__(self, exponent):
         n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
+#두 객체의 나누기
     def __truediv__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = self.num * pow(other.num, self.prime -2, self.prime) % self.prime
+        return self.__class__(num, self.prime)
+
+#int와 객체의 곱(서로 다른 type을 곱하기 위해)
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
         return self.__class__(num, self.prime)
--- src/Point.py
+++ src/Point.py
@@ -1,4 +1,5 @@
 class Point:
+#Point 객체에 내부값을 초기화(생성자)
     def __init__(self, x, y, a, b):
         self.x = x
         self.y = y
@@ -9,12 +10,15 @@ class Point:
         if self.y ** 2 != self.x ** 3 + self.a * self.x + self.b:
             raise ValueError('({}, {}) is not on the curve'.format(x, y))
 
+#두 객체가 같은지 확인
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
 
+#두 객체가 다른지 확인
     def __ne__(self, other):
         return not (self, other)
 
+#두 객체의 합
     def __add__(self, other):
         if self.a != other.a or self.b != other.b:
             raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
@@ -32,7 +36,20 @@ class Point:
         if self == other and self.y == 0 * self.x:
             return self.__class__(None, None, self.a, self.b)
         if self == other:
-            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            s = (3 * (self.x**2) + self.a) / (2 * self.y)
             x3 = s**2 - 2 * self.x
             y3 = s * (self.x - x3) - self.y
             return self.__class__(x3, y3, self.a, self.b)        
+
+#서로 type이 다른 int와 객체를 곱
+#스칼라 곱셈은 int형의 수와 Point 클래스형 객체의 곱셈 연산을 할 수 있는 함수 __rmul__()
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while coef:
+            if coef & 1:
+                result += current
+            current += current
+            coef >>= 1
+        return result 
