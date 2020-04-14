34b761b5983bcdc1ba2a725547c8e9b45bab9b8b
HippoMans <army1703@pukyong.ac.kr>
2020-04-15 00:42:27 +0900

    verack 메시지를 보내기 위한 VerAckMessage 클래스(작은 네트워크 메시지)

---
+++ src/VerAckMessage.py
@@ -0,0 +1,13 @@
+
+class VerAckMessage:
+    command = b'verack'
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def parse(cls, s):
+        return cls()
+
+    def serialize(self):
+        return b''
