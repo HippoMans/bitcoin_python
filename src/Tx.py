d90f5480d148cf8efb75f483039fd62675ee5d1d
HippoMans <army1703@pukyong.ac.kr>
2020-04-15 00:30:37 +0900

    트랜잭션을 표현하는 Tx클래스

--- src/Tx.py
+++ src/Tx.py
@@ -6,14 +6,21 @@ from lib.helper import read_varint
 from src.TxIn import TxIn
 from src.TxOut import TxOut
 from src.Script import Script
+from io import BytesIO
     
 class Tx:
-    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+    command = b'tx'
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False, segwit=False):
         self.version = version
         self.tx_ins = tx_ins         # 입력 생성자의 인수 tx_ins를 초기화
         self.tx_outs = tx_outs       # 출력 생성자의 인수 tx_outs를 초기화
         self.locktime = locktime
         self.testnet = testnet       # 트랜잭션을 검증하기 위해서는 트랜잭션의 네트워크 설정 필요
+        self.segwit = segwit
+        self._hash_prevouts = None
+        self._hash_sequence = None
+        self._hash_outputs = None
+
 
     def __repr__(self):
         tx_ins = ''
@@ -42,31 +49,92 @@ class Tx:
 # parse 메서드는 Tx 클래스의 인스턴스를 반환하기 때문에 클래스 매서드
     @classmethod
     def parse(cls, s, testnet=False):
-        version = little_endian_to_int(s.read(4))     # 현재 스트림 버퍼에 있는 4바이트를 읽어와 파싱 시작
+        s.read(4)
+        if s.read(4) == b'\x00':
+            parse_method = cls.parse_segwit
+        else:
+            parse_method = cls.parse_legacy
+        s.seek(-5, 1)
+        return parse_method(s, testnet=testnet)
+
+    #classmethod
+    def parse_legacy(cls, s, testnet=False):
+        version = little_endian_to_int(s.read(4))
         num_inputs = read_varint(s)
         inputs = []
         for _ in range(num_inputs):
             inputs.append(TxIn.parse(s))
-
-        outputs = []
         num_outputs = read_varint(s)
-        for _ in range (num_outputs):
-            outputs.append(TxOut.parse(s))           
+        outputs = []
+        for _ in range(num_outputs):
+            outputs.append(TxOut.parse(s))
         locktime = little_endian_to_int(s.read(4))
-        return cls(version, inputs, outputs, locktime, testnet=testnet)
+        return cls(version, inputs, outputs, locktime, testnet=testnet, segwit=False)
+
+    @classmethod
+    def parse_segwit(cls, s, testnet=False):
+        version = little_endian_to_int(s.read(4))
+        marker = s.read(2)
+        if marker != b'\x00\x01':
+            raise RuntimeError('Not a segwit transaction {}'.format(marker))
+        num_inputs = read_varint(s)
+        inputs = []
+        for _ in range(num_inputs):
+            inputs.append(TxIn.parse(s))
+        num_outputs = read_varint(s)
+        outputs = []
+        for _ in range(num_outputs):
+            outputs.append(TxOut.parse(s))
+        for tx_in in inputs:
+            num_items = read_varint(s)
+            items = []
+            for _ in range(num_items):
+                item_len = read_varint(s)
+                if item_len == 0:
+                    items.append(0)
+                else:
+                    items.append(s.read(item_len))
+            tx_in.witness = items
+        locktime = little_endian_to_int(s.read(4))
+        return cls(version, inputs, outputs, locktime, testnet=testnet, segwit=True)
 
 
 # 주어진 트랜잭션 직렬화하기
     def serialize(self):
-        result = int_to_little_endian(self.version, 4)      # 버전을 정수로 변경
-        result += encode_varint(len(self.tx_ins))           # 트랜잭션 입력 개수를 가변정수로 변경 추가
-        for tx_in in self.tx_ins:                           # 트랜잭션 입력 직렬화
+        if self.segwit:
+            return self.serialize_segwit()
+        else:
+            return self.serialize_legacy()
+
+    def serialize_legacy(self):
+        result = int_to_little_endian(self.version, 4)
+        result += encode_varint(len(self.tx_ins))
+        for tx_in in self.tx_ins:
             result += tx_in.serialize()
-        result += encode_varint(len(self.tx_outs))          # 트랜잭션 출력 갯수를 가변정수로 변경 추가
-        for tx_out in self.tx_outs:                         # 트랜잭션 출력 직렬화
+        result += encode_varint(len(self.tx_outs))
+        for tx_out in self.tx_outs:
             result += tx_out.serialize()
-        result += int_to_little_endian(self.locktime, 4)    # locktime 직렬화
-        # return bytes results(직렬화된 트랜잭션 객체)
+        result += int_to_little_endian(self.locktime, 4)
+        return result
+
+
+    def serialize_segwit(self):
+        result = int_to_little_endian(self.version, 4)
+        result += b'\x00\x01'
+        result += encode_varint(len(self.tx_ins))
+        for tx_in in self.tx_ins:
+            result += tx_in.serialize()
+        result += encode_varint(len(self.tx_outs))
+        for tx_out in self.tx_outs:
+            result += tx_out.serialize()
+        for tx_in in self.tx_ins:
+            result += int_to_little_endian(len(tx_in.witness), 1)
+            for item in tx_in.witness:
+                if type(item) == int:
+                    result += int_to_little_endian(item, 1)
+                else:
+                    result += encode_varint(len(item)) + item
+        result += int_to_little_endian(self.locktime, 4)
         return result
 
     def is_coinbase(self):
@@ -109,6 +177,7 @@ class Tx:
                 return False
         return True
 
+
     def sig_hash(self, input_index, redeem_script=None):
         s = int_to_little_endian(self.version, 4)
         s += encode_varint(len(self.tx_ins))
@@ -134,18 +203,84 @@ class Tx:
         h256 = hash256(s)
         return int.from_bytes(h256, 'big')
 
+    def hash_prevouts(self):
+        if self._hash_prevouts is None:
+            all_prevouts = b''
+            all_sequence = b''
+            for tx_in in self.tx_ins:
+                all_prevouts += tx_in.prev_tx[::-1] + int_to_little_endian(tx_in.prev_index, 4)
+                all_sequence += int_to_little_endian(tx_in.sequence, 4)
+            self._hash_prevouts = hash256(all_prevouts)
+            self._hash_sequence = hash256(all_sequence)
+        return self._hash_prevouts
+ 
+    def hash_sequence(self):
+        if self._hash_sequence is None:
+            self.hash_prevouts()
+        return self._hash_sequence
+
+    def hash_outputs(self):
+        if self._hash_outputs is None:
+            all_outputs = b''
+            for tx_out in self.tx_outs:
+                all_outputs += tx_out.serialize()
+            self._hash_outputs = hash256(all_outputs)
+        return self._hash_outputs 
+
+
+    def sig_hash_bip143(self, input_index, redeem_script=None, witness_script=None):
+        tx_in = self.tx_ins[input_index]
+        s = int_to_little__endian(self.version, 4)
+        s += self.hash_prevouts() + self.hash_sequence()
+        s += tx_in.prev_tx[::-1] + int_to_little_endian(tx_in.prev_index, 4)
+        if witness_script:
+            script_code = witness_script.serialize()
+        elif redeem_script:
+            script_code = p2pkh_script(redeem_script.cmds[1]).serialize()
+        else:
+            script_code = p2pkh_script(tx_in.script_pubkey(self.testnet).cmds[1]).serialize()
+        s += script_code
+        s += int_to_little_endian(tx_in.value(), 8)
+        s += int_to_little_endian(tx_in.sequence, 4)
+        s += self.hash_outputs()
+        s += int_to_little_endian(self.locktime, 4)
+        s += int_to_little_endian(SIGHASH_ALL, 4)
+        return int.from_bytes(hash256(s), 'big')
+
     def verify_input(self, input_index):
         tx_in = self.tx_ins[input_index]
         script_pubkey = tx_in.script_pubkey(testnet=self.testnet)
         if script_pubkey.is_p2sh_script_pubkey():
             cmd = tx_in.script_sig.cmds[-1]
-            raw_redeem = encode_varint(len(cmd)) + cmd
+            raw_redeem = int_to_little_endian(len(cmd)) + cmd
             redeem_script = Script.parse(BytesIO(raw_redeem))
+            if redeem_script.is_p2wpkh_script_pubkey():
+                z = self.sig_hash_bip143(input_index, redeem_script)
+                witness = tx_in.witness
+            elif redeem_script.is_p2wsh_script_pubkey():
+                cmd = tx_in.witness[-1]
+                raw_witness = encode_varint(len(cmd)) + cmd
+                witness_script = Script.parse(BytesIO(raw_witness))
+                z = self.sig_hash_bip143(input_index, witness_script=witness_script)
+                witness = tx_in.witness
+            else:
+                z = self.sig_hash(input_index, redeem_script)
+                witness = None
         else:
-            redeem_script = None
-        z = self.sig_hash(input_index, redeem_script)
+            if script_pubkey.is_p2wpkh_script_pubkey():
+                z = self.sig_hash_bip143(input_index)
+                witness = tx_in.witness
+            elif script_pubkey.is_p2wsh_script_pubkey():
+                cmd = tx_in.witness[-1]
+                raw_witness = encode_varint(len(cmd)) + cmd
+                witness_script = Script.parse(BytesIO(raw_witness))
+                z = self.sig_hash_bip143(input_index, witness_script=witness_script)
+                witness = tx_in.witness
+            else:
+                z = self.sig_hash(input_index)
+                witness = None
         combined = tx_in.script_sig + script_pubkey
-        return combined.evaluate(z)
+        return combined.evaluate(z, witness)
 
     def sign_input(self, input_index, private_key):
         z = self.sig_hash(input_index)
@@ -154,5 +289,4 @@ class Tx:
         sec = private_key.point.sec()
         self.tx_ins[input_index].script_sig = Script([sig, sec])
         return self.verify_input(input_index)
-
 
