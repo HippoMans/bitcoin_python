6dd3d8b3ca0bc6983f32c5d04e2d1c0cd3fe4c23
HippoMans <army1703@pukyong.ac.kr>
2020-04-15 00:34:05 +0900

    트랜잭션의 출력부분으로 다음 트랜잭션을 나타내는 TxOut클래스

--- src/TxOut.py
+++ src/TxOut.py
@@ -3,23 +3,23 @@ from lib.helper import int_to_little_endian
 from src.Script import Script
 
 class TxOut:
-    def __init__(self, amount, script_publickey):
+    def __init__(self, amount, script_pubkey):
         self.amount = amount
-        self.script_publickey = script_publickey
+        self.script_pubkey = script_pubkey
 
     def __repr__(self):
-        return '{}:{}'.format(self.amount, self.script_publickey)
+        return '{}:{}'.format(self.amount, self.script_pubkey)
 
     @classmethod
     def parse(cls, s):
         amount = little_endian_to_int(s.read(8))
-        script_publickey = Script.parse(s)
+        script_pubkey = Script.parse(s)
         '''returns a TxOut object'''
-        return cls(amount, script_publickey)
+        return cls(amount, script_pubkey)
        
 #트랜잭션 직렬화 코등 
     def serialize(slef):
         result = int_to_little_endian(self.amount, 8)
-        result += self.script_publickey.serialize()
+        result += self.script_pubkey.serialize()
         '''return the byte serialization of the transaction output'''
         return result
