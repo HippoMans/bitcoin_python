4247e1dd9b114141fbcaef61dc6b777ea6be53d4
HippoMans <army1703@pukyong.ac.kr>
2020-04-15 00:46:50 +0900

    블록 헤더를 응답하는 HeadersMessage 클래스

---
+++ src/HeadersMessage.py
@@ -0,0 +1,17 @@
+
+class HeadersMessage:
+    command = b'headers'
+
+    def __init__(self, blocks):
+        self.blocks = blocks
+
+    @classmethod
+    def parse(cls, stream):
+        num_headers = read_varint(stream)
+        blocks = []
+        for _ in range(num_headers):
+            blocks.append(Block.parse(stream))
+            num_txs = read_varint(stream)
+            if num_txs != 0:
+                raise RuntimeError('number of txs not 0')
+        return cls(blocks)
