d1846e5af8b0d25c54ede8d695aff238ccd35d61
HippoMans <army1703@pukyong.ac.kr>
2020-04-15 00:44:46 +0900

    블록 헤더를 요청하는 GetHeadersMessage 클래스

---
+++ src/GetHeadersMessage.py
@@ -0,0 +1,23 @@
+from lib.helper import int_to_little_endian
+from lib.helper import encode_varint
+
+class GetHeadersMessage:
+    command = b'getheaders'
+
+    def __init__(self, version=70015, num_hashes=1, start_block=None, end_block=None):
+        self.version = version
+        self.num_hashes = num_hashes
+        if start_block is None:
+            raise RuntimeError('a start block is required')
+        self.start_block = start_block
+        if end_block is None:
+            self.end_block = b'\x00' * 32
+        else:
+            self.end_block = end_block
+
+    def serialize(self):
+        result = int_to_little_endian(self.version, 4)
+        result += encode_varint(self.num_hashes)
+        result += self.start_block[::-1]
+        result += self.end_block[::-1]
+        return result 
